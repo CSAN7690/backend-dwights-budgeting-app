028c8fddeae17025f15f5db2034e9b6d92e5e2c0
CSAN7690 <christophersanchez@pursuit.org>
2024-07-04 19:09:17 -0400

    Created transactions.js with initial transactions data from Office staff

---
+++ src/model/transactions.js
@@ -0,0 +1,28 @@
+const transactions = [
+    {
+        id: 1,
+        item_name: 'Catering from Kevin',
+        amount: 200,
+        date: '2024-07-01',
+        from: 'Kevin Malone',
+        category: 'Food'
+    },
+    {
+        id: 2,
+        item_name: 'Decorations from Angela',
+        amount: 150,
+        date: '2024-07-02',
+        from: 'Angela Martin',
+        category: 'Decorations'
+    },
+    {
+        id: 3,
+        item_name: 'Party planning from Jim',
+        amount: 300,
+        date: '2024-07-03',
+        from: 'Jim Halpert',
+        category: 'Planning'
+    }
+];
+
+module.exports = transactions;
